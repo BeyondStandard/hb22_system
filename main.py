b05123ac653846b4308dd50908ca2bb95863f985
Icebox <mantas_kandratavicius@yahoo.com>
2022-07-08 03:58:59 +0300

    Training test run 3

--- main.py
+++ main.py
@@ -138,46 +138,6 @@ class AudioUtil:
         return spec
 
 
-def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
-    waveform = waveform.numpy()
-
-    num_channels, num_frames = waveform.shape
-    time_axis = torch.arange(0, num_frames) / sample_rate
-
-    figure, axes = plt.subplots(num_channels, 1)
-    if num_channels == 1:
-        axes = [axes]
-    for c in range(num_channels):
-        axes[c].plot(time_axis, waveform[c], linewidth=1)
-        axes[c].grid(True)
-        if num_channels > 1:
-            axes[c].set_ylabel(f'Channel {c+1}')
-        if xlim:
-            axes[c].set_xlim(xlim)
-        if ylim:
-            axes[c].set_ylim(ylim)
-    figure.suptitle(title)
-    plt.show(block=False)
-
-def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
-    waveform = waveform.numpy()
-
-    num_channels, num_frames = waveform.shape
-    time_axis = torch.arange(0, num_frames) / sample_rate
-
-    figure, axes = plt.subplots(num_channels, 1)
-    if num_channels == 1:
-        axes = [axes]
-    for c in range(num_channels):
-        axes[c].specgram(waveform[c], Fs=sample_rate)
-        if num_channels > 1:
-            axes[c].set_ylabel(f'Channel {c+1}')
-        if xlim:
-            axes[c].set_xlim(xlim)
-    figure.suptitle(title)
-    plt.show(block=False)
-
-
 # Sound Dataset
 class SoundDS(Dataset):
     def __init__(self, df):
@@ -362,5 +322,5 @@ def training(model, train_dl, num_epochs):
     print('Finished Training')
 
 
-num_epochs = 2
+num_epochs = 10
 training(myModel, train_dl, num_epochs)
