b86c1b7dd5c02a70da7f0604c2cdd399858859cf
Konstantin Seitz <konstantin.seitz@tchibo.de>
2022-07-08 19:03:21 +0200

    fix: filepath

--- app/api.py
+++ app/api.py
@@ -54,7 +54,7 @@ def get_audio(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @app.post("/ingest", response_model=AudioSchema)
 async def ingest_audio_b64(schema: AudioSchema, db: Session = Depends(get_db)):
     
-    new_audio = Audio(Audio.base64_to_filepath)
+    new_audio = Audio(Audio.base64_to_filepath(schema.audio_encoded))
     new_audio.preprocess()
     s = Spectrography(new_audio)
     s.spectro_augment()
