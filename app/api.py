08034e5a795c6689eb2ab63cd90e5de24e9ca4cc
Konstantin Seitz <konstantin.seitz@tchibo.de>
2022-07-08 19:30:19 +0200

    fix: init model

--- app/api.py
+++ app/api.py
@@ -55,6 +55,7 @@ def get_audio(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 async def ingest_audio_b64(schema: AudioSchema, db: Session = Depends(get_db)):
     
     model = Model()
+    model.initialize_from_file("./../Models/cloud_model_1.pt", True)
     result = model.server_process(schema.audio_encoded)
     println(result)
 
