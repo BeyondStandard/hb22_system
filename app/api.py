8e899ae4bf2204670531929cb5966f9add472c8f
Konstantin Seitz <konstantin.seitz@tchibo.de>
2022-07-08 19:07:09 +0200

    fix: keyword error

--- app/api.py
+++ app/api.py
@@ -54,13 +54,13 @@ def get_audio(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @app.post("/ingest", response_model=AudioSchema)
 async def ingest_audio_b64(schema: AudioSchema, db: Session = Depends(get_db)):
     
-    new_audio = Audio(Audio.base64_to_filepath(schema.audio_encoded))
+    new_audio = Audio(Audio.base64_to_filepath(schema))
     new_audio.preprocess()
     s = Spectrography(new_audio)
     s.spectro_augment()
     model = Model()
 
-    model.initialize_from_file("./../Models/cloud_model_1.pt", api=False)
+    model.initialize_from_file("./../Models/cloud_model_1.pt", False)
     result = model.classify(s, unsqueeze = True)
     print(result)
 
