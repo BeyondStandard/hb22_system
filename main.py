92fc403ecc6f88fc334db42cc41d6f55aa2a2884
Icebox <mantas_kandratavicius@yahoo.com>
2022-07-08 22:16:09 +0300

    Picking bugfix

--- main.py
+++ main.py
@@ -493,5 +493,7 @@ class AudioClassifier(nn.Module):
         return x
 
 
-if __name__ == '__main__':
-    model = Model()
+model = Model()
+m = torch.load(f'Models/cloud_model_1.pt')
+model.initialize_from_file('cloud_model_1')
+torch.save(model, f'Models/cloud_model_2.pt')
