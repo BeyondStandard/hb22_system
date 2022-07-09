fad921de671266fefb84e42584ac42d2037391a1
Konstantin Seitz <konstantin.seitz@tchibo.de>
2022-07-09 12:14:36 +0200

    try dict

--- app/database.py
+++ app/database.py
@@ -4,8 +4,76 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL =  'sqlite:///./sql.db'
 
+class JsonSerializableBase(object):
+
+    _json_include = []
+    _json_exclude = []
+
+    def __json__(self, excluded_keys=set()):
+        ins = inspect(self)
+
+        columns = set(ins.mapper.column_attrs.keys())
+        relationships = set(ins.mapper.relationships.keys())
+        unloaded = ins.unloaded
+        expired = ins.expired_attributes
+        include = set(self._json_include)
+        exclude = set(self._json_exclude) | excluded_keys
+
+        # This set of keys determines which fields will be present in
+        # the resulting JSON object.
+        # Here we initialize it with properties defined by the model class,
+        # and then add/delete some columns below in a tricky way.
+        keys = columns | relationships
+
+
+        # 1. Remove not yet loaded properties.
+        # Basically this is needed to serialize only .join()'ed relationships
+        # and omit all other lazy-loaded things.
+        if not ins.transient:
+            # If the entity is not transient -- exclude unloaded keys
+            # Transient entities won't load these anyway, so it's safe to
+            # include all columns and get defaults
+            keys -= unloaded
+
+        # 2. Re-load expired attributes.
+        # At the previous step (1) we substracted unloaded keys, and usually
+        # that includes all expired keys. Actually we don't want to remove the
+        # expired keys, we want to refresh them, so here we have to re-add them
+        # back. And they will be refreshed later, upon first read.
+        if ins.expired:
+            keys |= expired
+
+        # 3. Add keys explicitly specified in _json_include list.
+        # That allows you to override those attributes unloaded above.
+        # For example, you may include some lazy-loaded relationship() there
+        # (which is usually removed at the step 1).
+        keys |= include
+
+        # 4. For objects in `deleted` or `detached` state, remove all
+        # relationships and lazy-loaded attributes, because they require
+        # refreshing data from the DB, but this cannot be done in these states.
+        # That is:
+        #  - if the object is deleted, you can't refresh data from the DB
+        #    because there is no data in the DB, everything is deleted
+        #  - if the object is detached, then there is no DB session associated
+        #    with the object, so you don't have a DB connection to send a query
+        # So in both cases you get an error if you try to read such attributes.
+        if ins.deleted or ins.detached:
+            keys -= relationships
+            keys -= unloaded
+
+        # 5. Delete all explicitly black-listed keys.
+        # That should be done last, since that may be used to hide some
+        # sensitive data from JSON representation.
+        keys -= exclude
+
+        return { key: getattr(self, key)  for key in keys }
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = declarative_base(cls=(JsonSerializableBase,))
+
+
